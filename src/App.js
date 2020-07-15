9e6f7f5d14146885c20bf514fea2e88755b5af50
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-07-14 21:57:38 -0300

    Button left

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import Date from './components/Date';
+import BtnLeft from './components/BtnLeft';
 
 export default function App() {
   return (
     <div className='container center'>
       <h3>Personal Finances</h3>
-      <Date />
+      <div className='row'>
+        <BtnLeft />
+        <Date />
+      </div>
     </div>
   );
 }
---
+++ src/components/BtnLeft.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class BtnLeft extends Component {
+  render() {
+    return <div className='col s2'>Botão esquerda</div>;
+  }
+}
