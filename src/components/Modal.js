cc951721f0c810515704adccb6f5448aca82984b
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-07-23 15:57:21 -0300

    Adjustments modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -47,8 +47,32 @@ class Modal extends Component {
                         If you want Fixed Footer Modal then add
                         modal-fixed-footer to the "modal" div*/}
           <div className='modal-content'>
-            <h4>Modal Header</h4>
-            <p>A bunch of text</p>
+            <h4>Edição de Lançamento</h4>
+            <div style={styles.content}>
+              <form style={styles.form} className='col s12'>
+                <p>
+                  <label>
+                    <input name='group1' type='radio' checked />
+                    <span>Red</span>
+                  </label>
+                </p>
+                <p>
+                  <label>
+                    <input name='group1' type='radio' />
+                    <span>Yellow</span>
+                  </label>
+                </p>
+
+                <div className='input-field col s12'>
+                  <textarea id='description' className='materialize-textarea'></textarea>
+                  <label for='description'>Descrição</label>
+                </div>
+                <div className='input-field col s12'>
+                  <textarea id='category' className='materialize-textarea'></textarea>
+                  <label for='category'>Categoria</label>
+                </div>
+              </form>
+            </div>
           </div>
           <div className='modal-footer'>
             <i className='modal-close waves-effect waves-red btn-flat'>Disagree</i>
@@ -61,3 +85,16 @@ class Modal extends Component {
 }
 
 export default Modal;
+
+const styles = {
+  form: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  content: {
+    display: 'flex',
+    justifyContent: 'center',
+    border: '1px solid gray',
+    borderRadius: '5px',
+  },
+};
