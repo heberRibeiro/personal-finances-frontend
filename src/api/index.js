3f296a8a43892553feeac71678f4c57f8195e627
Heber Ribeiro <55813932+heberRibeiro@users.noreply.github.com>
2020-07-26 16:25:31 -0300

    Adjustment in import and export of the api module

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
-const http = require('./http-common').default;
+import http from './http-common';
 
-const getAll = () => {
+export const getAll = () => {
   return http.get('/api/transaction');
 };
 
-const get = async period => {
+export const get = async period => {
   try {
     const res = await http.get(`api/transaction/period?period=${period}`);
 
@@ -28,19 +28,19 @@ const get = async period => {
   }
 };
 
-const update = async (id, data) => {
+export const update = async (id, data) => {
   await http.put(`api/transaction/${id}`, data);
 };
 
-const remove = async id => {
+export const remove = async id => {
   await http.delete(`api/transaction/${id}`);
 };
 
-const create = async data => {
+export const create = async data => {
   await http.post('api/transaction/', data);
 };
 
-module.exports = {
+export default {
   getAll,
   get,
   update,
